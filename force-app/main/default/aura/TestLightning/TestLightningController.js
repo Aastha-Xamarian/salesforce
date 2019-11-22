0ea2f0de1a0c99143a9a38ecc4314cfe29ca811c
Aastha Jain <aasthaj@successive.com>
2019-11-22 17:04:52 +0530

    fourth

--- force-app/main/default/aura/TestLightning/TestLightningController.js
+++ force-app/main/default/aura/TestLightning/TestLightningController.js
@@ -1,5 +1,17 @@
 ({
-    myAction : function(component, event, helper) {
-
-    }
-})
+    createAccount : function(component, event) {
+      var newAcc = component.get("v.newAccount");
+      var action = component.get("c.saveAccount");
+      action.setParams({ 
+          "acc": newAcc
+      });
+      action.setCallback(this, function(a) {
+             var state = a.getState();
+              if (state === "SUCCESS") {
+                  var name = a.getReturnValue();
+                 alert("hello from here"+name);
+              }
+          });
+      $A.enqueueAction(action)
+  }
+  })
